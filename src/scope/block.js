4411e530795206eea2b51171632988d2305c2a65
Mateoferrer66 <mateoferrer66@gmail.com>
2023-01-24 13:34:42 -0500

    re_variables

--- src/scope/block.js
+++ src/scope/block.js
@@ -5,8 +5,8 @@ function fruits() {
         var one = 'a'; //function scope
         let two = 'b';  //block scope
         const three = 'c'; //block scope
-        console.log(two); //al estar en el bloque si imprime
-        console.log(three);//al estar en el bloque si imprime
+            console.log(two); //al estar en el bloque si imprime
+            console.log(three);//al estar en el bloque si imprime
     }       
     console.log(one); //al ser var no importa por ser global
     console.log(two);//error porque esta fuera de bloque
---
+++ src/scope/reasignation-declaration.js
@@ -0,0 +1,31 @@
+var firstName; //undefined solo esta definida, no asignada solo definida 
+
+firstName = 'Mateo'; //  asignar variable darle valor
+console.log(firstName);
+
+
+// var y let se puede reasignar 
+// const no puede reasignar
+
+//var puede declarar 
+// let y const no puede redeclarar
+
+// const no se puede reasignar por ser constante siempre tiene que ser unica 
+
+// Con var
+var nombre // declaración (undefined) ✅
+nombre = "Andres" // asignación ✅
+nombre = "Valeria" // reasignación ✅
+var nombre = "Oscar" // redeclaración y reasignación ✅
+
+// Con let
+let nombre // declaración (undefined) ✅
+nombre = "Andres" // asignación ✅
+nombre = "Valeria" // reasignación ✅
+let nombre = "Oscar" // SyntaxError: Identifier 'nombre' has already been declared. ❌
+
+// Con const 
+//const nombre // SyntaxError: Missing initializer in const declaration. ❌
+const nombre = "Andres" // declaración y asignación ✅
+nombre = "Valeria" // TypeError: Assignment to constant variable. ❌
+const nombre = "Oscar" // SyntaxError: Identifier 'nombre' has already been declared. ❌
